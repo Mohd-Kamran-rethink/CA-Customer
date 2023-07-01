eb8d9bbf226c7811cbf571d3e6e1996d11c46f6e
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-07-01 17:23:09 +0530

    changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -275,7 +275,7 @@ class UserController extends Controller
         $validator = Validator::make($req->all(), [
             // 'name' => 'required',
             // 'ca_id' => 'required|unique:clients,ca_id',
-            'number' => 'required',
+            'number' => 'required|unique:clients,number',
             'exchange' => 'required|not_in:0',
             'ca_id' => 'required|unique:clients,ca_id',
         ]);
